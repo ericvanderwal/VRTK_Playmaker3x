5aff65943e3e7e64d21b74f6dbe4c7c77ec13721
dumbgamedev <eric_vanderwal@hotmail.com>
2017-02-21 12:58:48 +0800

    Add files via upload

---
+++ Pointers/Renderers/StraightPointerCustomAppearance.cs
@@ -0,0 +1,77 @@
+﻿// Custom Action by DumbGameDev
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("VRTK")]
+	[Tooltip("Set Straight Pointer Custom Appearance.")]
+
+	public class  StraightPointerCustomAppearance : FsmStateAction
+	{
+		[RequiredField]
+		[CheckForComponent(typeof(VRTK.VRTK_StraightPointerRenderer))]    
+
+		public FsmOwnerDefault gameObject;
+
+		[TitleAttribute("Custom Tracer")]
+		public FsmGameObject tracer;
+
+		[TitleAttribute("Custom Cursor")]
+		public FsmGameObject cursor;
+
+		public FsmBool everyFrame;
+
+	
+
+
+		VRTK.VRTK_StraightPointerRenderer theScript;
+
+		public override void Reset()
+		{
+
+			gameObject = null;
+			cursor = null;
+			tracer = null;
+			everyFrame = false;
+		}
+
+		public override void OnEnter()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+
+
+			theScript = go.GetComponent<VRTK.VRTK_StraightPointerRenderer>();
+
+			if (!everyFrame.Value)
+			{
+				MakeItSo();
+				Finish();
+			}
+
+		}
+
+		public override void OnUpdate()
+		{
+			if (everyFrame.Value)
+			{
+				MakeItSo();
+			}
+		}
+
+
+		void MakeItSo()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go == null)
+			{
+				return;
+			}
+
+			theScript.customCursor = cursor.Value;
+			theScript.customTracer = tracer.Value;
+
+		}
+
+	}
+}
