28e9a91831885728053dd4d5c866854fbecc836c
dumbgamedev <eric_vanderwal@hotmail.com>
2017-02-24 01:25:29 +0800

    Add files via upload

---
+++ Scripts/SetBodyPhysicsEnableTeleport.cs
@@ -0,0 +1,67 @@
+﻿// Custom Action by DumbGameDev
+// www.dumbgamedev.com
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("VRTK")]
+	[Tooltip("Enable teleport settings for Body Physics for VRTK.")]
+
+	public class  SetBodyPhysicsEnableTeleport : FsmStateAction
+
+	{
+		[RequiredField]
+		[CheckForComponent(typeof(VRTK.VRTK_BodyPhysics))]    
+		public FsmOwnerDefault gameObject;
+
+		public FsmBool enableTeleport;
+
+		public FsmBool everyFrame;
+
+		VRTK.VRTK_BodyPhysics theScript;
+
+		public override void Reset()
+		{
+			enableTeleport = true;
+			gameObject = null;
+			everyFrame = false;
+		}
+
+		public override void OnEnter()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+
+			theScript = go.GetComponent<VRTK.VRTK_BodyPhysics>();
+
+			if (!everyFrame.Value)
+			{
+				MakeItSo();
+				Finish();
+			}
+
+		}
+
+		public override void OnUpdate()
+		{
+			if (everyFrame.Value)
+			{
+				MakeItSo();
+			}
+		}
+
+
+		void MakeItSo()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go == null)
+			{
+				return;
+			}
+
+			theScript.enableTeleport = enableTeleport.Value;
+
+		}
+
+	}
+}
