18e2836e752e8c95550e78b11aaa8e643a23e6e4
dumbgamedev <eric_vanderwal@hotmail.com>
2017-02-24 12:41:59 +0800

    Add files via upload

---
+++ TriggerEvents/GetTriggerPressed.cs
@@ -0,0 +1,68 @@
+﻿// Custom Action by DumbGameDev
+// www.dumbgamedev.com
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("VRTKController")]
+	[Tooltip("Get trigger pressed for VRTK.")]
+
+	public class  GetTriggerPressed : FsmStateAction
+
+	{
+		[RequiredField]
+		[CheckForComponent(typeof(VRTK.VRTK_ControllerEvents))]    
+		public FsmOwnerDefault gameObject;
+
+		public FsmBool triggerPressed;
+
+		public FsmBool everyFrame;
+
+		VRTK.VRTK_ControllerEvents theScript;
+
+		public override void Reset()
+		{
+
+			triggerPressed = false;
+			gameObject = null;
+			everyFrame = false;
+		}
+
+		public override void OnEnter()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+
+			theScript = go.GetComponent<VRTK.VRTK_ControllerEvents>();
+
+			if (!everyFrame.Value)
+			{
+				MakeItSo();
+				Finish();
+			}
+
+		}
+
+		public override void OnUpdate()
+		{
+			if (everyFrame.Value)
+			{
+				MakeItSo();
+			}
+		}
+
+
+		void MakeItSo()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go == null)
+			{
+				return;
+			}
+
+			triggerPressed.Value = theScript.triggerPressed;
+
+		}
+
+	}
+}
