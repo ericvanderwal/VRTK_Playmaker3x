bbf737c9b659194d5b676fa4047b309e6e1ecc29
dumbgamedev <eric_vanderwal@hotmail.com>
2017-03-01 20:25:40 +0800

    Add files via upload

---
+++ Pointer/SetPointerDestinationMarkerSettings.cs
@@ -0,0 +1,68 @@
+﻿// Custom Action by DumbGameDev
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("VRTK")]
+	[Tooltip("Set Pointer Destination Marker Settings.")]
+
+	public class  SetPointerDestinationMarkerSettings : FsmStateAction
+	{
+		[RequiredField]
+		[CheckForComponent(typeof(VRTK.VRTK_Pointer))]    
+
+		public FsmOwnerDefault gameObject;
+
+		[TitleAttribute("Enable Teleport")]
+		public FsmBool enTeleport;
+
+		public FsmBool everyFrame;
+
+		VRTK.VRTK_Pointer theScript;
+
+		public override void Reset()
+		{
+
+			gameObject = null;
+			enTeleport = true;
+			everyFrame = false;
+		}
+
+		public override void OnEnter()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+
+
+			theScript = go.GetComponent<VRTK.VRTK_Pointer>();
+
+			if (!everyFrame.Value)
+			{
+				MakeItSo();
+				Finish();
+			}
+
+		}
+
+		public override void OnUpdate()
+		{
+			if (everyFrame.Value)
+			{
+				MakeItSo();
+			}
+		}
+
+
+		void MakeItSo()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go == null)
+			{
+				return;
+			}
+
+			theScript.enableTeleport = enTeleport.Value;
+		}
+
+	}
+}
---
+++ Pointer/SetPointerInteractionSettings.cs
@@ -0,0 +1,75 @@
+﻿// Custom Action by DumbGameDev
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("VRTK")]
+	[Tooltip("Set Pointer Interaction Settings.")]
+
+	public class  SetPointerInteractionSettings : FsmStateAction
+	{
+		[RequiredField]
+		[CheckForComponent(typeof(VRTK.VRTK_Pointer))]    
+
+		public FsmOwnerDefault gameObject;
+
+		[TitleAttribute("Interact with Object")]
+		public FsmBool grabTip;
+
+		[TitleAttribute("Grab to Pointer Tip")]
+		public FsmBool interObj;
+
+		public FsmBool everyFrame;
+
+		VRTK.VRTK_Pointer theScript;
+
+		public override void Reset()
+		{
+
+			gameObject = null;
+			grabTip = null;
+			interObj = null;
+			everyFrame = false;
+		}
+
+		public override void OnEnter()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+
+
+			theScript = go.GetComponent<VRTK.VRTK_Pointer>();
+
+			if (!everyFrame.Value)
+			{
+				MakeItSo();
+				Finish();
+			}
+
+		}
+
+		public override void OnUpdate()
+		{
+			if (everyFrame.Value)
+			{
+				MakeItSo();
+			}
+		}
+
+
+		void MakeItSo()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go == null)
+			{
+				return;
+			}
+
+			theScript.interactWithObjects = interObj.Value;
+			theScript.grabToPointerTip = grabTip.Value;
+
+		}
+
+	}
+}
+
